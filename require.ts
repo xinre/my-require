a6939532dc5fcf0bc53fa187be1ce752f5a4d0b5
tanghongyu <505648237@qq.com>
2020-06-27 15:38:40 +0800

    push

--- require.ts
+++ require.ts
@@ -1,2 +1,19 @@
 const path = require('path');
 const fs = require('fs');
+
+interface ModuledDefinition {
+  id: string
+  exports: {}
+}
+
+class Module implements ModuledDefinition {
+  public id
+  public exports
+
+  constructor(outId: string) { this.id = outId }
+}
+
+function Require (filePath: string): void {
+  let absolutePath = path.resolve(__dirname, filePath)
+  let module = new Module(filePath)
+}
